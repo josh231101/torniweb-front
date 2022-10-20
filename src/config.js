2ca04baed8f458271c513f86483259e966cd3d04
Josué Arroeola <wiijosue333@gmail.com>
2022-10-20 09:46:55 -0500

    Fix token ep call

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
-  // PROD: homeUrl: 'https://josue.ngrok.io',
-  homeUrl: 'http://127.0.0.1:3333',
+  // PROD
+  homeUrl: 'https://josue.ngrok.io',
+  //homeUrl: 'http://127.0.0.1:3333',
   defaultTimezone: 'America/Mexico_City',
   imagesUrl: 'https://torniweb.s3.amazonaws.com/'
 }
